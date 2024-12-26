d534df92c430e4fe423e616cab82903971e95ba2
Vadzim <VadzimKamkou@yandex.com>
2024-12-26 14:32:12 +0300

    Fonts Added

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,10 +1,18 @@
 import UIKit
 
 final class MovieQuizViewController: UIViewController {
+ 
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    
+    
+    
+    
+    
+    
 }
 
 /*
