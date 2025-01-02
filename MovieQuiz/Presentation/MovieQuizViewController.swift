7b64f295204803390ed3b2d08cb35e3a90dfaba5
Vadzim <VadzimKamkou@yandex.com>
2025-01-02 14:37:44 +0300

    Show Questions Added

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,21 +1,144 @@
 import UIKit
 import Foundation
 
+
+
+
+
+
+
+
 final class MovieQuizViewController: UIViewController {
  
-    // Outlets
-   
+    @IBOutlet weak private var counterLabel: UILabel!
+    @IBOutlet weak private var imageView: UIImageView!
+    @IBOutlet weak private var textLabel: UILabel!
     
-    // MARK: - Lifecycle
+
+    // массив вопросов
+    struct QuizQuestion {
+      // строка с названием фильма,
+      // совпадает с названием картинки афиши фильма в Assets
+      let image: String
+      // строка с вопросом о рейтинге фильма
+      let text: String
+      // булевое значение (true, false), правильный ответ на вопрос
+      let correctAnswer: Bool
+    }
+
+    private let questions: [QuizQuestion] = [
+        QuizQuestion(image: "The Godfather", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+        QuizQuestion(image: "The Dark Knight", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+        QuizQuestion(image: "Kill Bill", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+        QuizQuestion(image: "The Avengers", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+        QuizQuestion(image: "Deadpool", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+        QuizQuestion(image: "The Green Knight", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+        QuizQuestion(image: "Old", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false),
+        QuizQuestion(image: "The Ice Age Adventures of Buck Wild", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false),
+        QuizQuestion(image: "Tesla", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false),
+        QuizQuestion(image: "Vivarium", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false)
+    ]
+    
+    // вью модель для состояния "Вопрос показан"
+    struct QuizStepViewModel {
+      // картинка с афишей фильма с типом UIImage
+      let image: UIImage
+      // вопрос о рейтинге квиза
+      let question: String
+      // строка с порядковым номером этого вопроса (ex. "1/10")
+      let questionNumber: String
+    }
+
+    // индекс текущего вопроса, начальное значение 0
+    private var currentQuestionIndex = 0
+    // cчётчик правильных ответов, начальное значение 0
+    private var correctAnswers = 0
+    
+    // модификации после загрузки приложения
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        //buttonYes.tintColor = .ypWhite
-       // buttonYes.setTitleColor(.ypBlack, for: .normal)
-       // buttonYes.setTitleColor(.ypBlack, for: .selected)
+        
+        // выводим первый вопрос
+        let currentQuestion = questions[currentQuestionIndex]
+        let currentQuestionViewModel = convert(model: currentQuestion)
+        show(quiz: currentQuestionViewModel)
+       
         
     }
- 
+    
+    // конвертирует моковый вопрос и возвращает ViewModel
+    private func convert(model: QuizQuestion) -> QuizStepViewModel {
+        // создаем константу и вызываем конструктор
+        let questionStep = QuizStepViewModel(
+            // инициализируем картинку
+            image: UIImage(named: model.image) ?? UIImage(),
+            // забираем вопрос из моковых данных
+            question: model.text,
+            // высчитываем номер вопроса
+            questionNumber: "\(currentQuestionIndex + 1)/\(questions.count)")
+        // возвращаем объект структуры
+        return questionStep
+    }
+    
+    // вывода на экран вопроса по ViewModel
+    private func show(quiz step: QuizStepViewModel) {
+          // попробуйте написать код показа на экран самостоятельно
+        counterLabel.text = step.questionNumber
+        imageView.image = step.image
+        textLabel.text = step.question
+    }
+        
+
+    @IBAction private func noButtonClicked(_ sender: UIButton) {
+        
+        let currentQuestion = questions[currentQuestionIndex]
+        let givenAnswer = false
+        showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
+    }
+    
+    @IBAction private func yesButtonClicked(_ sender: UIButton) {
+        let currentQuestion = questions[currentQuestionIndex]
+        let givenAnswer = true
+        showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
+    }
+    
+    // обрабатываем ответ пользователя
+    private func showAnswerResult(isCorrect: Bool) {
+       // метод красит рамку
+        imageView.layer.masksToBounds = true // даём разрешение на рисование рамки
+        imageView.layer.borderWidth = 8 // толщина рамки
+        imageView.layer.cornerRadius = 20 // радиус скругления углов рамки
+        
+        if isCorrect == true {
+            imageView.layer.borderColor = UIColor.ypGreen.cgColor
+        } else {
+            imageView.layer.borderColor = UIColor.ypRed.cgColor
+        }
+        
+        // запускаем задачу через 1 секунду c помощью диспетчера задач
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+           self.showNextQuestionOrResults()
+        }
+    }
+    
+    // приватный метод, который содержит логику перехода в один из сценариев
+    // метод ничего не принимает и ничего не возвращает
+    private func showNextQuestionOrResults() {
+        
+       
+        
+        if currentQuestionIndex == questions.count - 1 {
+            
+        } else {
+            currentQuestionIndex += 1
+            
+            let currentQuestion = questions[currentQuestionIndex]
+            let currentQuestionViewModel = convert(model: currentQuestion)
+            show(quiz: currentQuestionViewModel)
+            
+            
+        }
+    }
     
 }
 
